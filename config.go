eb041dd450a8eb91dff9bcf66f65f28069e3fb27
Salar Moghaddam <salarmgh@gmail.com>
2020-05-05 17:39:11 +0430

    Add config support

---
+++ config.go
@@ -0,0 +1,30 @@
+package gomicrosvc
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Configuration struct {
+	Rabbitmq struct {
+		Host     string `yaml:"host"`
+		User     string `yaml:"user"`
+		Password string `yaml:"password"`
+		Exchange string `yaml:"exchange"`
+	} `yaml:"rabbitmq"`
+}
+
+var config Configuration
+
+func initConfig() {
+	file, err := ioutil.ReadFile("config.yml")
+	if err != nil {
+		panic(err)
+	}
+
+	err = yaml.Unmarshal(file, &config)
+	if err != nil {
+		panic(err)
+	}
+}
--- initialize.go
+++ initialize.go
@@ -1,21 +1,28 @@
 package gomicrosvc
 
-var Connection = GetConn("amqp://guest:guest@localhost")
+import "fmt"
+
+var Connection Conn
 
 func Initialize() {
+	initConfig()
+
+	Connection = GetConn(fmt.Sprintf("amqp://%s:%s@%s", config.Rabbitmq.User,
+		config.Rabbitmq.Password, config.Rabbitmq.Host))
+
 	ch, err := GetChannel(&Connection)
 	if err != nil {
 		panic(err)
 	}
 
 	err = ch.Channel.ExchangeDeclare(
-		"rpc-bus", // name
-		"topic",   // type
-		true,      // durable
-		false,     // auto-deleted
-		false,     // internal
-		false,     // no-wait
-		nil,       // arguments
+		config.Rabbitmq.Exchange, // name
+		"topic",                  // type
+		true,                     // durable
+		false,                    // auto-deleted
+		false,                    // internal
+		false,                    // no-wait
+		nil,                      // arguments
 	)
 
 	if err != nil {
