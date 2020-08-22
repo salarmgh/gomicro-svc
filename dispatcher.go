28a98656157fe0abf3f01dae156423688f06dcc5
Salar Moghaddam <salarmgh@gmail.com>
2020-08-22 17:48:52 +0430

    Concurrent dispatcher

--- dispatcher.go
+++ dispatcher.go
@@ -10,15 +10,29 @@ import (
 )
 
 func dispatcher(message amqp.Delivery) bool {
-	method := strings.Split(message.RoutingKey, ".")[1]
-	if handler, ok := Handlers[method]; ok {
-		msg := Message{}
-		err := proto.Unmarshal(message.Body, &msg)
-		result, err := handler(msg.Result)
-		if err != nil {
-			log.Println(err)
-			res := anypb.Any{}
-			data := Message{Result: &res, Error: err.Error()}
+	go func() {
+		method := strings.Split(message.RoutingKey, ".")[1]
+		if handler, ok := Handlers[method]; ok {
+			msg := Message{}
+			err := proto.Unmarshal(message.Body, &msg)
+			result, err := handler(msg.Result)
+			if err != nil {
+				log.Println(err)
+				res := anypb.Any{}
+				data := Message{Result: &res, Error: err.Error()}
+				marshalledData, err := proto.Marshal(&data)
+				if err != nil {
+					log.Println(err)
+					res := anypb.Any{}
+					data := Message{Result: &res, Error: "Couldn't Marshal"}
+					marshalledData, _ := proto.Marshal(&data)
+					err = Publish(message.ReplyTo, message.CorrelationId, &marshalledData)
+					//return true
+				}
+				err = Publish(message.ReplyTo, message.CorrelationId, &marshalledData)
+				//return true
+			}
+			data := Message{Result: result, Error: ""}
 			marshalledData, err := proto.Marshal(&data)
 			if err != nil {
 				log.Println(err)
@@ -26,25 +40,13 @@ func dispatcher(message amqp.Delivery) bool {
 				data := Message{Result: &res, Error: "Couldn't Marshal"}
 				marshalledData, _ := proto.Marshal(&data)
 				err = Publish(message.ReplyTo, message.CorrelationId, &marshalledData)
-				return true
+				//return true
 			}
+			log.Printf("Sent reply to %s, corrID: %s", message.ReplyTo, message.CorrelationId)
 			err = Publish(message.ReplyTo, message.CorrelationId, &marshalledData)
-			return true
+			//return true
 		}
-		data := Message{Result: result, Error: ""}
-		marshalledData, err := proto.Marshal(&data)
-		if err != nil {
-			log.Println(err)
-			res := anypb.Any{}
-			data := Message{Result: &res, Error: "Couldn't Marshal"}
-			marshalledData, _ := proto.Marshal(&data)
-			err = Publish(message.ReplyTo, message.CorrelationId, &marshalledData)
-			return true
-		}
-		log.Printf("Sent reply to %s, corrID: %s", message.ReplyTo, message.CorrelationId)
-		err = Publish(message.ReplyTo, message.CorrelationId, &marshalledData)
-		return true
-	}
+	}()
 
 	return true
 }
